07509ba625b12b16470b590c912892af79458e04
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2024-06-12 21:29:15 -0500

    Create 6.py

---
+++ TAREAS/VPC_ALTA_DISPONIBILIDAD/TEORIA/6.py
@@ -0,0 +1,45 @@
+import boto3
+
+# Configurar cliente de AWS para NLB y GWLB
+elbv2_client = boto3.client('elbv2', region_name='us-east-1')
+network_firewall_client = boto3.client('network-firewall', region_name='us-east-1')
+
+# Crear un Network Load Balancer (NLB)
+response_nlb = elbv2_client.create_load_balancer(
+    Name='MyNLB',
+    Subnets=['subnet-12345678', 'subnet-87654321'],
+    Scheme='internet-facing',
+    Type='network',
+    IpAddressType='ipv4',
+    Tags=[
+        {
+            'Key': 'Name',
+            'Value': 'MyNLB'
+        },
+    ]
+)
+
+# Crear un Gateway Load Balancer (GWLB)
+response_gwlb = network_firewall_client.create_firewall(
+    FirewallName='MyGWLB',
+    FirewallPolicyArn='arn:aws:network-firewall:us-east-1:123456789012:stateful-rule-group/applications/MyFirewallPolicy',
+    VpcId='vpc-12345678',
+    SubnetMappings=[
+        {
+            'SubnetId': 'subnet-12345678',
+        },
+        {
+            'SubnetId': 'subnet-87654321',
+        },
+    ],
+    Tags=[
+        {
+            'Key': 'Name',
+            'Value': 'MyGWLB'
+        },
+    ]
+)
+
+# Imprimir las respuestas de creación
+print("NLB creado:", response_nlb)
+print("GWLB creado:", response_gwlb)
