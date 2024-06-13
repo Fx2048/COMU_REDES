7e7dc4ed14b5c5402d99366c690327253f3250b3
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2024-06-12 21:31:48 -0500

    Update 9.py

--- TAREAS/VPC_ALTA_DISPONIBILIDAD/TEORIA/9.py
+++ TAREAS/VPC_ALTA_DISPONIBILIDAD/TEORIA/9.py
@@ -36,3 +36,45 @@ response = autoscaling.create_auto_scaling_group(**auto_scaling_group)
 
 # Imprimir la respuesta
 print("Grupo de Auto Scaling creado:", response)
+
+
+# Configurar la plantilla de lanzamiento (Launch Template)
+launch_template = {
+    'LaunchTemplateName': 'my-launch-template',
+    'Version': '1',
+    'LaunchTemplateData': {
+        'ImageId': 'ami-87654321',
+        'InstanceType': 't3.micro',
+        'KeyName': 'my-key-pair',
+        'SecurityGroupIds': ['sg-87654321'],
+        'UserData': 'base64-encoded-user-data'
+    }
+}
+
+# Crear la plantilla de lanzamiento
+response = autoscaling.create_launch_template(**launch_template)
+
+# Utilizar la plantilla de lanzamiento en un nuevo grupo de Auto Scaling
+auto_scaling_group_with_template = {
+    'AutoScalingGroupName': 'my-auto-scaling-group-with-template',
+    'LaunchTemplate': {
+        'LaunchTemplateName': 'my-launch-template',
+        'Version': '1'
+    },
+    'MinSize': 2,
+    'MaxSize': 5,
+    'DesiredCapacity': 2,
+    'AvailabilityZones': ['us-east-1a', 'us-east-1b'],
+    'HealthCheckType': 'EC2',
+    'HealthCheckGracePeriod': 300,
+    'Tags': [
+        {'Key': 'Name', 'Value': 'my-asg-instance-with-template'}
+    ]
+}
+
+# Crear el grupo de Auto Scaling utilizando la plantilla de lanzamiento
+response = autoscaling.create_auto_scaling_group(**auto_scaling_group_with_template)
+
+# Imprimir la respuesta
+print("Grupo de Auto Scaling con plantilla creado:", response)
+
