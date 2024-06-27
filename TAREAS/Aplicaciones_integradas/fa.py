916ac71b52ca6c4e9cfcb4a8c08eb69ddb58917b
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2024-06-27 11:08:10 -0500

    Add files via upload

---
+++ TAREAS/Aplicaciones_integradas/fa.py
@@ -0,0 +1,94 @@
+import threading
+import time
+
+class Message:
+    def __init__(self, topic, content):
+        self.topic = topic
+        self.content = content
+        self.timestamp = time.time()
+
+class Topic:
+    def __init__(self, name):
+        self.name = name
+        self.subscribers = []
+
+    def add_subscriber(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def remove_subscriber(self, subscriber):
+        self.subscribers.remove(subscriber)
+
+    def get_subscribers(self):
+        return self.subscribers
+
+class Subscriber:
+    def __init__(self, name):
+        self.name = name
+
+    def notify(self, message):
+        print(f"Subscriber {self.name} received message on topic '{message.topic}': {message.content}")
+
+class Publisher:
+    def __init__(self, broker):
+        self.broker = broker
+
+    def publish(self, topic_name, content):
+        message = Message(topic_name, content)
+        self.broker.publish_message(topic_name, message)
+
+class Broker:
+    def __init__(self):
+        self.topics = {}
+
+    def create_topic(self, topic_name):
+        if topic_name not in self.topics:
+            self.topics[topic_name] = Topic(topic_name)
+        else:
+            raise Exception(f"Topic '{topic_name}' already exists")
+
+    def subscribe(self, topic_name, subscriber):
+        if topic_name in self.topics:
+            self.topics[topic_name].add_subscriber(subscriber)
+        else:
+            raise Exception(f"Topic '{topic_name}' does not exist")
+
+    def unsubscribe(self, topic_name, subscriber):
+        if topic_name in self.topics:
+            self.topics[topic_name].remove_subscriber(subscriber)
+        else:
+            raise Exception(f"Topic '{topic_name}' does not exist")
+
+    def publish_message(self, topic_name, message):
+        if topic_name in self.topics:
+            subscribers = self.topics[topic_name].get_subscribers()
+            for subscriber in subscribers:
+                threading.Thread(target=subscriber.notify, args=(message,)).start()
+        else:
+            raise Exception(f"Topic '{topic_name}' does not exist")
+
+# Caso de uso
+def main():
+    broker = Broker()
+
+    # Crear tópicos
+    broker.create_topic("Sports")
+    broker.create_topic("News")
+
+    # Crear suscriptores
+    subscriber1 = Subscriber("Hiato")
+    subscriber2 = Subscriber("Diptongo")
+
+    # Suscribir suscriptores a tópicos
+    broker.subscribe("Sports", subscriber1)
+    broker.subscribe("News", subscriber1)
+    broker.subscribe("News", subscriber2)
+
+    # Crear publicador
+    publisher = Publisher(broker)
+
+    # Publicar mensajes
+    publisher.publish("Sports", "Encuentro de futbol hoy a las 5AM")
+    publisher.publish("News", "Noticias de momento: Nuevas ofertas de productos")
+
+if __name__ == "__main__":
+    main()
